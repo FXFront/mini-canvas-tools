a8824d55c7a2a7cd497d9bb1d2075f1e92c583d2
qiuxchao <2915241235@qq.com>
2024-02-20 17:55:19 +0800

    🔧 build: 打包优化

--- build.config.ts
+++ build.config.ts
@@ -1,7 +1,7 @@
-import { rename, writeFileSync } from 'fs-extra';
+import { writeFileSync } from 'fs-extra';
 import path from 'node:path';
 import { defineBuildConfig } from 'unbuild';
-import {transformFileSync} from '@swc/core';
+import { transformFileSync } from '@swc/core';
 
 export default defineBuildConfig({
   entries: ['src/index'],
@@ -9,22 +9,21 @@ export default defineBuildConfig({
   declaration: true,
   clean: true,
   rollup: {
-    emitCJS: true,
     inlineDependencies: true,
     esbuild: {
       // minify: true,
-      target: 'node12',
+      format: 'esm',
     },
   },
   failOnWarn: false,
   hooks: {
     'build:done': async () => {
-      const code = transformFileSync(path.resolve(__dirname, 'lib/index.cjs'), {
+      const output = transformFileSync(path.resolve(__dirname, 'lib/index.mjs'), {
         jsc: {
           target: 'es3',
         },
-      })
-      writeFileSync(path.resolve(__dirname, 'lib/index.js'), code.code);
+      });
+      writeFileSync(path.resolve(__dirname, 'lib/index.js'), output.code);
     },
   },
 });
